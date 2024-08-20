538b5beb6d6047310c8088af8958418d2f20014f
Maksim Jaroslavcevas <36482401+Maksasj@users.noreply.github.com>
2024-08-20 16:49:13 +0300

    Tag model

---
+++ Qilin.Service/Models/TagModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Qilin.Service.Models
+{
+    public class TagModel
+    {
+        [Required]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
