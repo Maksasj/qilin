bb706549c7c482ccecac69743d9496d7b97ac7b3
Maksim Jaroslavcevas <radioboos@gmail.com>
2024-09-01 20:11:52 +0300

    creation of new tags

--- src/components/tags/create-tag-window.tsx
+++ src/components/tags/create-tag-window.tsx
@@ -11,6 +11,7 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Textarea } from "../ui/textarea"
 import axios from "axios"
+import React from "react"
 
 interface Props {
 	open: boolean
@@ -18,6 +19,9 @@ interface Props {
 }
 
 export function CreateTagWindow(props: Props) {
+	const [title, setTitle] = React.useState("New Tag");
+	const [description, setDescription] = React.useState<string>("");
+
 	return (
 		<Dialog open={props.open} onOpenChange={props.setOpen}>
 			<DialogContent className="sm:max-w-[425px]">
@@ -36,21 +40,26 @@ export function CreateTagWindow(props: Props) {
 							id="name"
 							defaultValue="New Tag"
 							className="col-span-3"
+							onChange={(event) => {
+								setTitle(event.target.value)
+							}}
 						/>
 					</div>
 					<div className="grid grid-cols-4 items-center gap-4">
 						<Label htmlFor="username" className="text-right">
 							Tag Description
 						</Label>
-						<Textarea className="nue-create-tag-window-textarea" />
+						<Textarea className="nue-create-tag-window-textarea" onChange={(event) => {
+							setDescription(event.target.value)
+						}} />
 					</div>
 				</div>
 				<DialogFooter>
 					<Button onClick={async () => {
 						var formData = new FormData();
 
-						formData.append('tagTitle', 'New tag');
-						formData.append('tagDescription', 'Cool description');
+						formData.append('tagTitle', title);
+						formData.append('tagDescription', description);
 
 						await axios({
 							method: "post",
