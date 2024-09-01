9919123b1aa4dcf7f25fced9a1e10d2ea4dc29ac
Maksim Jaroslavcevas <radioboos@gmail.com>
2024-09-01 15:54:39 +0300

    new badge design

--- src/components/tags/tag-badge.tsx
+++ src/components/tags/tag-badge.tsx
@@ -1,9 +1,13 @@
+import { Circle, CircleX } from 'lucide-react';
 import './tag-badge.css'
 
 const TagBadge = ({ color, label }: { color: string, label: string }) => {
 	return (
-		<div style={{ backgroundColor: color }} className="nue-tag-badge">
-			{label}
+		<div style={{ backgroundColor: color }} className="nue-tag-badge-base">
+			<div className="nue-tag-badge-name">
+				{label}
+			</div>
+			<CircleX className="nue-tag-badge-remove" />
 		</div>
 	);
 };
