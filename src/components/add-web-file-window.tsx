e870d1c33ac8903e108987a2bbdc5136775a09f6
Maksim Jaroslavcevas <radioboos@gmail.com>
2024-09-01 20:35:13 +0300

    working add web file window

--- src/components/add-web-file-window.tsx
+++ src/components/add-web-file-window.tsx
@@ -1,47 +1,65 @@
 import { Button } from "@/components/ui/button"
 import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
 } from "@/components/ui/dialog"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import axios from "axios"
+import React from "react"
 
 interface Props {
-  open: boolean
-  setOpen: (arg: boolean) => void
+	open: boolean
+	setOpen: (arg: boolean) => void
 }
 
 export function AddWebFileWindow(props: Props) {
-  return (
-    <Dialog open={props.open} onOpenChange={props.setOpen}>
-      <DialogContent className="sm:max-w-[425px]">
-        <DialogHeader>
-          <DialogTitle>Add Web File</DialogTitle>
-          <DialogDescription>
-            Add a new web file
-          </DialogDescription>
-        </DialogHeader>
-        <div className="grid gap-4 py-4">
-          <div className="grid grid-cols-4 items-center gap-4">
-            <Label htmlFor="name" className="text-right">
-              File Access Url
-            </Label>
-            <Input
-              id="name"
-              className="col-span-3"
-            />
-          </div>
-        </div>
-        <DialogFooter>
-          <Button onClick={() => {
-            props.setOpen(false);
-          }} type="submit">Add new web file</Button>
-        </DialogFooter>
-      </DialogContent>
-    </Dialog>
-  )
+	const [fileUrl, setFileUrl] = React.useState("");
+
+	return (
+		<Dialog open={props.open} onOpenChange={props.setOpen}>
+			<DialogContent className="sm:max-w-[425px]">
+				<DialogHeader>
+					<DialogTitle>Add Web File</DialogTitle>
+					<DialogDescription>
+						Add a new web file
+					</DialogDescription>
+				</DialogHeader>
+				<div className="grid gap-4 py-4">
+					<div className="grid grid-cols-4 items-center gap-4">
+						<Label htmlFor="name" className="text-right">
+							File Access Url
+						</Label>
+						<Input
+							id="name"
+							className="col-span-3"
+							onChange={(event) => {
+								setFileUrl(event.target.value)
+							}}
+						/>
+					</div>
+				</div>
+				<DialogFooter>
+					<Button onClick={async () => {
+						var formData = new FormData();
+
+						formData.append('fileUri', fileUrl);
+
+						await axios({
+							method: "post",
+							url: "https://localhost:7256/AddWebFile",
+							data: formData,
+							headers: { "Content-Type": "multipart/form-data" },
+						})
+
+						props.setOpen(false);
+					}}>Add new web file</Button>
+				</DialogFooter>
+			</DialogContent>
+		</Dialog>
+	)
 }
