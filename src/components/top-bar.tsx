27ae636051215004cd208a6ec5a3086850c2548d
Maksim Jaroslavcevas <radioboos@gmail.com>
2024-09-02 17:39:44 +0300

    base for tag searching

--- src/components/top-bar.tsx
+++ src/components/top-bar.tsx
@@ -6,9 +6,13 @@ import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
 import './top-bar.css'
 import React from "react";
 import { PopoverAnchor } from "@radix-ui/react-popover";
-import { CommandInput } from "./ui/command";
+import { CommandInput, CommandSeparator } from "./ui/command";
 import { Tag } from "@/models/tag";
 import axios from "axios";
+import { TagsPageResponseModel } from "@/models/tags-page-response-model";
+import TagBadge from "./tags/tag-badge";
+import { Separator } from "@radix-ui/react-separator";
+import { CirclePlus, Plus } from "lucide-react";
 
 const Logo = () => {
 	return (
@@ -18,25 +22,38 @@ const Logo = () => {
 	);
 }
 
-const SearchInput = () => {
+const TagSearchInput = ({ callback }: { callback: (tag: Tag) => void }) => {
 	const [tmp, setTmp] = React.useState<string>("");
 	const [autoComplete, setAutoComplete] = React.useState<boolean>(true);
 
-	const [state, setState] = React.useState<Tag[]>([]);
+	const [state, setState] = React.useState<TagsPageResponseModel>();
 	const [isLoading, setIsLoading] = React.useState(true);
 
 	React.useEffect(() => {
-		getAllInformation()
+		if (tmp !== "")
+			getAllInformation()
+
 	}, [tmp]);
 
 	const getAllInformation = async () => {
 		setIsLoading(true);
 
-		await axios.get("https://localhost:7283/SearchTag?searchTitle=" + tmp).then(response => {
+		await axios.get("https://localhost:7283/SearchTag?searchTitle=" + tmp + "&maxCount=10").then(response => {
 			setIsLoading(false);
-			const allData: Tag[] = response.data;
+			const allData: TagsPageResponseModel = response.data;
 			setState(allData);
 		})
+	};
+
+	const handleBlur = (event: any) => {
+		if (
+			event.relatedTarget &&
+			(event.relatedTarget.classList.contains('no-blur') || event.relatedTarget.id === 'specific-element')
+		) {
+			return;
+		}
+
+		setAutoComplete(false);
 	};
 
 	return (
@@ -45,16 +62,71 @@ const SearchInput = () => {
 				className="nue-search-input"
 				type="text"
 				onFocus={() => setAutoComplete(true)}
-				onBlur={() => setAutoComplete(false)}
+				onBlur={(e) => handleBlur(e)}
 				onChange={(event) => {
-					setTmp(event.target.value)
+					setTmp(event.target.value);
 				}}
 				placeholder="Type a tag or search by multiple..." />
 
-			<div className="nue-search-auto-complete-base">
-				{tmp}
+			{autoComplete && (
+				<div className="no-blur nue-search-auto-complete-base" onMouseDown={(e) => e.preventDefault()} >
+					<div className="no-blur nue-search-auto-complete-base-label" onMouseDown={(e) => e.preventDefault()} >
+						Suggestions
+					</div>
+
+					<hr />
+
+					{state !== undefined && (
+						state.tags.map(tag => {
+							return (
+								<div className="no-blur nue-search-auto-complete-result" onMouseDown={(e) => e.preventDefault()} >
+									<TagBadge tag={tag} />
+									<div className="no-blur nue-search-auto-complete-result-description" onMouseDown={(e) => e.preventDefault()} >
+										{tag.value.description}
+									</div>
+									<div className="no-blur nue-search-auto-complete-result-button" onMouseDown={(e) => e.preventDefault()} >
+										<CirclePlus
+											className="no-blur"
+											onMouseDown={(e) => e.preventDefault()}
+											onClick={() => { callback(tag.value) }} />
+									</div>
+								</div>
+							)
+						})
+					)}
+
+					<hr />
+
+					{state !== undefined ? (
+						<div className="nue-search-auto-complete-result-description">
+							{state.itemCount} results
+						</div>
+					) : (
+						<div className="nue-search-auto-complete-result-description">
+							0 results
+						</div>
+					)}
+				</div>
+			)}
+		</div >
+	);
+}
+
+const SearchInput = () => {
+	return (
+		<div className="nue-search" contentEditable="true">
+			<div>
+				<div contentEditable="false">"poggers"
+
+				</div>
+				<p>
+					DKASNKDASDASDASDASFKFNASLJKGBNASLJKG
+				</p>
 			</div>
 
+			<TagSearchInput callback={(tag: Tag) => {
+				console.log("Poggers !" + tag.title);
+			}} />
 			<Button variant="outline">Search</Button>
 		</div >
 	);
